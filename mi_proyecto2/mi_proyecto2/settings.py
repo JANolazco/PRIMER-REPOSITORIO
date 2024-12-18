a53cb7438eefa5cbb3c87ff53ff970344ad353e4
JANolazco <aleperezaquino98@gmail.com>
2024-12-17 18:12:19 -0600

    ha habido una modifcacion, donde se le agrego otra aplicacion

--- mi_proyecto2/mi_proyecto2/settings.py
+++ mi_proyecto2/mi_proyecto2/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mi_aplicacion'
+    'mi_aplicacion',
+    'blog' ,
 ]
 
 MIDDLEWARE = [
